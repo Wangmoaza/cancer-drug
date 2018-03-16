1872ddb64321f81e6e6282a8caf66bae1f4f7f2a
Wangmoaza <haeun0512@hanmail.net>
2018-03-16 16:59:10 +0900

    parse gctx practice

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 This project finds cancer-drug relationships.
 
+# Date
+
+* 2018-03-16
+	* Learning to parse .gctx and .gct files
+	* Tutorial working on iPython notebook.
+
--- data_merge.py
+++ data_merge.py
@@ -1,3 +1,7 @@
 import pandas as pd
 import numpy as np
-import cmappy
+import cmapPy.pandasGEXpress as cmap
+
+def main():
+    my = cmap.parse.parse("../data/CCLE/CCLE_expression_CN_muts_GENEE_2010-04-16.gctx")
+    print py
